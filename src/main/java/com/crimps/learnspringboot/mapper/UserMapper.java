34f5f2e8b7356ed4ba9b0ccd0e8f12968b7cbcbe
crimps <woolpack.cwz@gmail.com>
2017-11-14 09:23:25 +0800

    注解版mybatis

---
+++ src/main/java/com/crimps/learnspringboot/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.crimps.learnspringboot.mapper;
+
+import com.crimps.learnspringboot.domain.UserEntity;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * Created by crimps on 2017/11/13.
+ */
+public interface UserMapper {
+
+    @Select("select * from user")
+    List<UserEntity> getAll();
+
+    @Select("select * from user where id = #{id}")
+    UserEntity getOne(String id);
+
+    @Insert("insert into user(id, username, password, email, nickname, regtime) " +
+            "values(#{id}, #{username}, #{password}, #{email}, #{nickname}, #{regtime})")
+    void insert(UserEntity userEntity);
+
+    @Update("update user set password = #{password} where id = #{id}")
+    void updatePassword(UserEntity userEntity);
+
+    @Delete("delete from user where id = #{id}")
+    void delete(String id);
+}
---
+++ src/test/java/com/crimps/learnspringboot/mapper/UserMapperTest.java
@@ -0,0 +1,67 @@
+package com.crimps.learnspringboot.mapper;
+
+import com.crimps.learnspringboot.domain.UserEntity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by crimps on 2017/11/13.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserMapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void getAll() throws Exception {
+        List<UserEntity> userEntityList = userMapper.getAll();
+        for (UserEntity userEntity : userEntityList) {
+            System.out.println(userEntity.getUsername());
+        }
+    }
+
+    @Test
+    public void getOne() {
+        UserEntity userEntity = userMapper.getOne("4d91c54d-429d-4e6d-a55d-02310394b37a");
+        System.out.println(userEntity.getUsername());
+    }
+
+    @Test
+    public void insert() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(UUID.randomUUID().toString());
+        userEntity.setUsername("tea bag");
+        userEntity.setPassword("3");
+        userEntity.setEmail("tea@bag.com");
+        userEntity.setNickname("TT");
+        userEntity.setRegtime(new Date().toString());
+        userMapper.insert(userEntity);
+    }
+
+    @Test
+    public void updatePassword() {
+        List<UserEntity> userEntityList = userMapper.getAll();
+        for(int i = 0; i < userEntityList.size(); i++) {
+            UserEntity userEntity = userEntityList.get(i);
+            userEntity.setPassword(String.valueOf(i));
+            userMapper.updatePassword(userEntity);
+        }
+    }
+
+    @Test
+    public void delete() {
+        List<UserEntity> userEntityList = userMapper.getAll();
+        if (!userEntityList.isEmpty()) {
+            userMapper.delete(userEntityList.get(0).getId());
+        }
+    }
+}
